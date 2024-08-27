05cab9bfc7cbb8e439202338b9b3a55e12843049
Rahul Tripathi <rahultripathidev@gmail.com>
2024-08-27 14:48:56 +0530

    ft: update config

--- examples/yieldagg/config/config.go
+++ examples/yieldagg/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"context"
+	"log"
+
+	"github.com/sethvargo/go-envconfig"
+)
+
+type Config struct {
+	ExecutorKey    string `env:"EXECUTOR_KEY"`
+	BaseRpcURL     string `env:"BASE_RPC_URL"`
+	MorphoBaseURL  string `env:"MORPHO_BASE_URL"`
+	ConsoleBaseURL string `env:"CONSOLE_BASE_URL"`
+}
+
+func LoadConfig(ctx context.Context) (*Config, error) {
+	c := &Config{}
+	if err := envconfig.Process(ctx, c); err != nil {
+		log.Fatal(err)
+	}
+
+	return c, nil
+}

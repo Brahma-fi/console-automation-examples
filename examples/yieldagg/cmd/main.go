688501f19ee9365729252cc50a26e90abc290b30
Rahul Tripathi <rahultripathidev@gmail.com>
2024-08-29 17:46:11 +0530

    ft: wire app

--- examples/yieldagg/cmd/main.go
+++ examples/yieldagg/cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+
+	"github.com/Brahma-fi/console-automation-examples/internal/app"
+)
+
+func main() {
+	err := app.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- examples/yieldagg/internal/app/app.go
+++ examples/yieldagg/internal/app/app.go
@@ -1,1 +1,31 @@
 package app
+
+import (
+	"context"
+
+	"github.com/Brahma-fi/console-automation-examples/config"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func Run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg, err := config.LoadConfig(ctx)
+	if err != nil {
+		return err
+	}
+
+	is, err := newIntegrationStorage(cfg)
+	if err != nil {
+		return err
+	}
+
+	ss, err := newServiceStorage(ctx, cfg, is)
+	if err != nil {
+		return err
+	}
+
+	addr := common.HexToAddress("")
+	return ss.Strategy.Run(ctx, addr, false)
+}

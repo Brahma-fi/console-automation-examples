5572b9821f851d2facd21d508e978ea882d30348
Rahul Tripathi <rahultripathidev@gmail.com>
2024-08-29 17:44:47 +0530

    ft: update config

--- examples/yieldagg/config/config.go
+++ examples/yieldagg/config/config.go
@@ -3,8 +3,37 @@ package config
 import (
 	"context"
 	"log"
+	"os"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/joho/godotenv"
 	"github.com/sethvargo/go-envconfig"
+)
+
+const (
+	BaseChainID           = 8453
+	SafeMultiSendCallOnly = "0x40a2accbd92bca938b02010e17a5b8929b49130d"
+)
+
+var (
+	SafeMultiSendABI, _ = abi.JSON(strings.NewReader(`
+		[
+		  {
+			"inputs": [
+			  {
+				"internalType": "bytes",
+				"name": "transactions",
+				"type": "bytes"
+			  }
+			],
+			"name": "multiSend",
+			"outputs": [],
+			"stateMutability": "payable",
+			"type": "function"
+		  }
+		]
+	`))
 )
 
 type Config struct {
@@ -16,6 +45,12 @@ type Config struct {
 
 func LoadConfig(ctx context.Context) (*Config, error) {
 	c := &Config{}
+	if check := os.Getenv("EXECUTOR_KEY"); check == "" {
+		if err := godotenv.Load(); err != nil {
+			return nil, err
+		}
+	}
+
 	if err := envconfig.Process(ctx, c); err != nil {
 		log.Fatal(err)
 	}
